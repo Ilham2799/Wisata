8f9872de4536c49157dddb0ebfa605a31f64da0b
Ilham2799 <muhammadilham270199@gmail.com>
2023-05-12 22:17:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/MUHAMAD_ILHAM_201011401698/Masjid.java
@@ -0,0 +1,14 @@
+package com.example.MUHAMAD_ILHAM_201011401698;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Masjid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masjid);
+    }
+}
